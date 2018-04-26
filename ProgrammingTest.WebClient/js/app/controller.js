970c5421252b7efa1ecc2d3be2791b1128c3836a
Sean <smccarthy94@users.noreply.github.com>
2018-04-27 09:28:32 +1000

    Update controller.js
    comment spelling mistakes

--- ProgrammingTest.WebClient/js/app/controller.js
+++ ProgrammingTest.WebClient/js/app/controller.js
@@ -14,24 +14,24 @@
                 $scope.Busy = false;
             }).catch(function (err) {
                 $scope.Busy = false;
-                // could consume Controll ModelState validation error info here...
+                // could consume Controller ModelState validation error info here...
             });
         };
 
         $scope.AddMember = function (member) {
 
             $scope.Busy = true;
-            $scope.Members.push(member); // pro-actively insert the row - makes the app looks faster :)
+            $scope.Members.push(member); // proactively insert the row - makes the app looks faster :)
 
             members.create(member).then(function (response) {
                 $scope.Busy = false;
-                $scope.Members.pop(); //remove and re-add the record, it will have an ID and created data now.
+                $scope.Members.pop(); //remove and re-add the record, it will have an ID and created date now.
                 $scope.Members.push(response.data);
                 $scope.NewMember = {};
             }).catch(function (err) {
                 $scope.Members.pop(); // remove it only on failure
                 $scope.Busy = false;
-                // could consume Controll ModelState validation error info here...
+                // could consume Controller ModelState validation error info here...
             })
         };
 
