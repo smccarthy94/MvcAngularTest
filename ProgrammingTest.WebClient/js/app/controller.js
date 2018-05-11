29640613d880bf52dd32bb6af9fee20042e738e0
Sean <smccarthy94@users.noreply.github.com>
2018-05-11 11:08:39 +1000

    Added $setPristine() call to form

--- ProgrammingTest.WebClient/js/app/controller.js
+++ ProgrammingTest.WebClient/js/app/controller.js
@@ -40,6 +40,17 @@
                 
                 // clear the object used by the page form.
                 $scope.NewMember = {};
+                
+                // set the form in the view to $pristine state
+                $scope.newForm.$setPristine();
+                // this tells the form that it should appear as untouched, without this line all fields within
+                // the form will show as being in an error state (as all fields are 'required' and have just been cleared).
+                //
+                // When the form is in an untouched state, it doesn't show 'required' error messages on fields
+                // until a user either interacts with a field and intentionally leaves it blank or hit submit too early.
+                //
+                // Note that we can iteract with the form element via the $scope using the 'name' that was provided
+                // on the form element in the view.
             }).catch(function (err) {
                 $scope.Members.pop(); // remove it only on failure.
                 $scope.Busy = false;
