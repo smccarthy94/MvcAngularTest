643f47ea78f416ca8e228394a4cb7b09378f723b
Sean <smccarthy94@users.noreply.github.com>
2018-05-11 10:52:49 +1000

    Comments for parameter data types in service.js

--- ProgrammingTest.WebClient/js/app/service.js
+++ ProgrammingTest.WebClient/js/app/service.js
@@ -1,9 +1,9 @@
 ﻿app.service('members', ['$http', function ($http) {
     return {
         list: function ()           { return $http.get('/api/members') },
-        get: function (toGet)       { return $http.get('/api/members/' + toGet) },
-        delete: function (toDelete) { return $http.delete('/api/members/' + toDelete) },
-        update: function (toUpdate) { return $http.put('/api/members', toUpdate) },
-        create: function (toCreate) { return $http.post('/api/members', toCreate) },
+        get: function (toGet)       { return $http.get('/api/members/' + toGet) }, // toGet, number
+        delete: function (toDelete) { return $http.delete('/api/members/' + toDelete) }, // toDelete, number
+        update: function (toUpdate) { return $http.put('/api/members', toUpdate) }, // toUpdate, object
+        create: function (toCreate) { return $http.post('/api/members', toCreate) }, // toCreate, object
     }
 }]);
