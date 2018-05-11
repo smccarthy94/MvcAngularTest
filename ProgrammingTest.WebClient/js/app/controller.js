b683c8533ce0d7e8271cd203c1bbb715a3b081c3
Sean <smccarthy94@users.noreply.github.com>
2018-05-11 10:58:08 +1000

    Comments improved in controller.js

--- ProgrammingTest.WebClient/js/app/controller.js
+++ ProgrammingTest.WebClient/js/app/controller.js
@@ -1,13 +1,17 @@
 ﻿app.controller('memberLister',
-    ['$scope', 'members', function ($scope, members) {
+    ['$scope', 'members', function ($scope, members) { // load the service we created in members.js.
 
+        // init all of the scope variables used in the markup.
         $scope.Members      = [];
         $scope.NewMember    = {};
         $scope.Busy         = false;
         $scope.FilterString = '';
 
+        // define refresh members function on the scope so it can be called from the view.
         $scope.RefreshMembersList = function () {
-            $scope.Busy = true;
+            
+            // to be consumed by the markup so certain elements can be disabled while requests are in progress.
+            $scope.Busy = true; 
 
             members.list().then(function (response) {
                 $scope.Members = response.data;
@@ -18,22 +22,31 @@
             });
         };
 
+        // define add member function on the scope so it can be called from the view.
         $scope.AddMember = function (member) {
-
+            
+            // to be consumed by the markup so certain elements can be disabled while requests are in progress.
             $scope.Busy = true;
-            $scope.Members.push(member); // proactively insert the row - makes the app looks faster :)
+            
+            // proactively insert the row - makes the app looks faster :)
+            $scope.Members.push(member); 
 
             members.create(member).then(function (response) {
                 $scope.Busy = false;
-                $scope.Members.pop(); //remove and re-add the record, it will have an ID and created date now.
+                
+                // remove and re-add the record, it will have an ID and created date now.
+                $scope.Members.pop(); 
                 $scope.Members.push(response.data);
+                
+                // clear the object used by the page form.
                 $scope.NewMember = {};
             }).catch(function (err) {
-                $scope.Members.pop(); // remove it only on failure
+                $scope.Members.pop(); // remove it only on failure.
                 $scope.Busy = false;
                 // could consume Controller ModelState validation error info here...
             })
         };
 
+        // kick off an initial refresh so the markup can be updated.
         $scope.RefreshMembersList();
     }]);
